cfd2575861c2893c04cb1bb7eca72b65e432f945
Dam0n01 <146678773+Dam0n01@users.noreply.github.com>
2025-08-03 22:17:23 +0000

    Lab 5

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea604d3cb8e3c6fc743ccc052be3a82c38ecf7c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6fff46913b3a2579db819c98318da8e8209c3f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace Lab5;
-
-class Program
+﻿namespace Lab5
 {
-    static void Main(string[] args)
+    // step 1 - Base class
+    class Animal
     {
-        Console.WriteLine("Hello, World!");
+        public virtual void AnimalSound()
+        {
+            Console.WriteLine("Animal Makes Sound");
+        }
+    }
+    // step 2- Derived classes
+    class Cat : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("The Cat says: Meow");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("The Dog says: Bow wow");
+        }
+    }
+    // step 3 - Test in Main
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            myAnimal.AnimalSound();
+
+            Cat myCat = new Cat();
+            myCat.AnimalSound();
+
+            Dog myDog = new Dog();
+            myDog.AnimalSound();
+
+
+
+        }
+
     }
 }
